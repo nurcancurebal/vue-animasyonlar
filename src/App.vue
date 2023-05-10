7608f268e0d263aa2873596b46ebe6c07346626e
nurcancurebal <nurcancurebal@hotmail.com>
2023-05-11 00:44:45 +0300

    animasyon2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="demo">
-    <button @click="show = !show">
-      Toggle
-    </button>
-    <transition name="fade">
-      <p v-if="show">hello</p>
+    <button @click="show = !show">Toggle show</button>
+    <transition name="bounce">
+      <p
+        v-if="show"
+      >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis enim libero, at lacinia diam fermentum id. Pellentesque habitant morbi tristique senectus et netus.</p>
     </transition>
   </div>
 </template>
@@ -20,11 +20,21 @@ export default {
 </script>
 
 <style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
 }
-.fade-enter, .fade-leave-to {
-  opacity: 0;
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
